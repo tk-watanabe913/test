53dad8cda975e2006b40d541835f4afadc2d6c8c
tk-watanabe913 <tkwatanabe913@gmail.com>
2019-06-21 18:12:00 +0900

    s3デプロイテスト

---
+++ test.py
@@ -0,0 +1,3 @@
+print ("Hellow world!77755")
+print ("Hellow world!777555555")
+print ("Hellow world!")
---
+++ test_sum.py
@@ -0,0 +1,24 @@
+
+import unittest
+
+
+
+from sum import sum
+
+
+
+class TestSum(unittest.TestCase):
+
+    def test_sum_001(self):
+
+        data = [1, 2, 3]
+
+        result = sum(data)
+
+        self.assertEqual(result, 6)
+
+
+
+if __name__ == '__main__':
+
+    unittest.main()
